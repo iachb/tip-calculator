acb4ceaeadef932893820d2a52be0407bbfc56bb
iachb <ivanchavezbrb@gmail.com>
2024-03-20 13:27:26 -0600

    Listing menu products

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-
+import { menuItems } from "./data/db";
+import MenuItem from "./components/MenuItem";
 
 function App() {
-
-
   return (
     <>
-      <h1>Hello World</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black ">Tips Calculator</h1>
+      </header>
+      <main className="max-w-7xl mx-auto mt-20 py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menu</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem
+                key={item.id}
+                item={item}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumption</h2>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
